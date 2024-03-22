8d15093547fa8503acfe172fc8535477282e7561
Ayaz Mubarak <ayaz.mubarakov@gmail.com>
2024-03-22 14:40:53 +0300

    recovery design fix

--- src/app/auth/recovery/Recovery.tsx
+++ src/app/auth/recovery/Recovery.tsx
@@ -3,20 +3,14 @@
 import NextLink from 'next/link'
 import React from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
-import {
-	Button,
-	CssBaseline,
-	TextField,
-	Box,
-	Typography,
-	Container
-} from '@mui/material'
+import { Button, TextField, Box, Typography, Avatar } from '@mui/material'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { IAuthRecoveryForm } from '@/types/auth.types'
 import { useMutation } from '@tanstack/react-query'
 import { toast } from 'sonner'
 import { authService } from '@/services/auth.service'
 import { errorCatch } from '@/api/error'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 
 export default function Recovery() {
 	const searchParams = useSearchParams()
@@ -46,112 +40,110 @@ export default function Recovery() {
 	}
 
 	return (
-		<Container
-			component='main'
-			maxWidth='xs'
+		<Box
+			sx={{
+				my: 8,
+				mx: 4,
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center'
+			}}
 		>
-			<CssBaseline />
-			<Box
-				sx={{
-					marginTop: 8,
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center'
-				}}
+			<Avatar sx={{ m: 1, bgcolor: '#9c07ab' }}>
+				<LockOutlinedIcon />
+			</Avatar>
+			<Typography
+				component='h1'
+				variant='h5'
 			>
-				<Typography
-					component='h1'
-					variant='h5'
-				>
-					{token ? 'Восстановить пароль' : 'Забыли пароль?'}
-				</Typography>
+				{token ? 'Восстановить пароль' : 'Забыли пароль?'}
+			</Typography>
 
-				<Box
-					component='form'
-					onSubmit={handleSubmit(onSubmit)}
-					noValidate
-					sx={{ mt: 1 }}
-				>
-					{token ? (
-						<>
-							<TextField
-								{...register('password', {
-									required: 'Не может быть пустым',
-									minLength: {
-										value: 8,
-										message: 'В пароле должно быть не менее 8 символов.'
-									},
-									pattern: {
-										value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
-										message:
-											'Пароль должен содержать по крайней мере одну цифру, одну строчную и одну заглавную букву'
-									}
-								})}
-								type='password'
-								error={!!errors.password}
-								label='Введите новый пароль'
-								helperText={errors.password?.message}
-								variant='outlined'
-								margin='normal'
-								size='small'
-								fullWidth
-								required
-							/>
-							<TextField
-								{...register('confirm_password', {
-									required: 'Не может быть пустым',
-									validate: {
-										matchesPassword: value =>
-											value === watch('password') || 'Пароли не совпадают'
-									}
-								})}
-								type='password'
-								error={!!errors.confirm_password}
-								label='Подтвердите новый пароль'
-								helperText={errors.confirm_password?.message}
-								variant='outlined'
-								margin='normal'
-								size='small'
-								fullWidth
-								required
-							/>
-						</>
-					) : (
+			<Box
+				component='form'
+				onSubmit={handleSubmit(onSubmit)}
+				noValidate
+				sx={{ mt: 1, maxWidth: '505px', width: '100%' }}
+			>
+				{token ? (
+					<>
 						<TextField
-							{...register('email', {
+							{...register('password', {
 								required: 'Не может быть пустым',
+								minLength: {
+									value: 8,
+									message: 'В пароле должно быть не менее 8 символов.'
+								},
 								pattern: {
-									value: /^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$/,
-									message: 'Пожалуйста, введите действующий Email-адрес.'
+									value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+									message:
+										'Пароль должен содержать по крайней мере одну цифру, одну строчную и одну заглавную букву'
 								}
 							})}
-							error={!!errors.email}
-							label='Email'
-							helperText={errors.email?.message}
+							type='password'
+							error={!!errors.password}
+							label='Введите новый пароль'
+							helperText={errors.password?.message}
 							variant='outlined'
 							margin='normal'
 							size='small'
 							fullWidth
 							required
 						/>
-					)}
-
-					<Button
-						type='submit'
+						<TextField
+							{...register('confirm_password', {
+								required: 'Не может быть пустым',
+								validate: {
+									matchesPassword: value =>
+										value === watch('password') || 'Пароли не совпадают'
+								}
+							})}
+							type='password'
+							error={!!errors.confirm_password}
+							label='Подтвердите новый пароль'
+							helperText={errors.confirm_password?.message}
+							variant='outlined'
+							margin='normal'
+							size='small'
+							fullWidth
+							required
+						/>
+					</>
+				) : (
+					<TextField
+						{...register('email', {
+							required: 'Не может быть пустым',
+							pattern: {
+								value: /^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$/,
+								message: 'Пожалуйста, введите действующий Email-адрес.'
+							}
+						})}
+						error={!!errors.email}
+						label='Email'
+						helperText={errors.email?.message}
+						variant='outlined'
+						margin='normal'
+						size='small'
 						fullWidth
-						variant='contained'
-					>
-						Сбросить пароль
-					</Button>
+						required
+					/>
+				)}
 
-					<NextLink
-						href='/auth'
-						className='Link'
-					>
-						Войти
-					</NextLink>
-				</Box>
+				<Button
+					type='submit'
+					fullWidth
+					variant='contained'
+				>
+					Сбросить пароль
+				</Button>
+
+				<NextLink
+					href='/auth'
+					className='Link'
+				>
+					Войти
+				</NextLink>
 			</Box>
-		</Container>
+		</Box>
 	)
 }
