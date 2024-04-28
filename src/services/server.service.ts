4602094a40039de9cf930924ecfdb14c3377846f
Ayaz Mubarak <ayaz.mubarakov@gmail.com>
2024-04-28 18:49:40 +0300

    middleware fix

--- src/services/server.service.ts
+++ src/services/server.service.ts
@@ -1,11 +1,11 @@
 export const addUserToWS = async (access: string, invite_token: string) => {
 	try {
-		const res = await fetch(process.env.URL_API + '/add_user', {
+		const res = await fetch(process.env.URL_API + '/workspaces/members/', {
 			method: 'POST',
 			headers: {
 				Authorization: `Bearer ${access}`
 			},
-			body: JSON.stringify({ invite_token })
+			body: invite_token
 		})
 		return res
 	} catch (error) {
