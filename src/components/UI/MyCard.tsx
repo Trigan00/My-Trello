f8d5eda6d1e13ed6e26e298a325b4e1de22fc704
Ayaz Mubarak <ayaz.mubarakov@gmail.com>
2024-07-19 13:24:11 +0300

    Card style fix

--- src/components/UI/MyCard.tsx
+++ src/components/UI/MyCard.tsx
@@ -1,5 +1,5 @@
 import { COLORS } from '@/constants/color.constants'
-import { Box, BoxProps, SxProps } from '@mui/material'
+import { Box, BoxProps, Card, SxProps } from '@mui/material'
 
 interface MyCardI extends BoxProps {
 	children: React.ReactNode
@@ -9,20 +9,29 @@ interface MyCardI extends BoxProps {
 
 export function MyCard({ children, variant, sx, ...props }: MyCardI) {
 	return (
-		<Box
-			{...props}
+		// <Box
+		// 	{...props}
+		// 	sx={{
+		// 		boxShadow:
+		// 			variant === 'shadowed'
+		// 				? '0px 0px 10px 0px rgba(0, 0, 0, 0.15)'
+		// 				: 'none',
+		// 		backgroundColor: 'white',
+		// 		border: variant === 'outlined' ? `1px solid ${COLORS.border}` : 'none',
+		// 		borderRadius: '15px',
+		// 		...sx
+		// 	}}
+		// >
+		// 	{children}
+		// </Box>
+		<Card
+			variant='outlined'
 			sx={{
-				boxShadow:
-					variant === 'shadowed'
-						? '0px 0px 10px 0px rgba(0, 0, 0, 0.15)'
-						: 'none',
-				backgroundColor: 'white',
-				border: variant === 'outlined' ? `1px solid ${COLORS.border}` : 'none',
 				borderRadius: '15px',
 				...sx
 			}}
 		>
 			{children}
-		</Box>
+		</Card>
 	)
 }
