71d483a2327d293f67be54ed734ca486dc096eb0
Ayaz Mubarak <ayaz.mubarakov@gmail.com>
2024-08-04 12:16:51 +0300

    trash removed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,21 +1,8 @@
-import dynamic from 'next/dynamic'
 import { Metadata } from 'next'
-// import Description from './[workspace_id]/boards/[board_id]/task_info/Description'
-const Description = dynamic(
-	() => import('./[workspace_id]/boards/[board_id]/task_info/Description'),
-	{
-		ssr: false
-	}
-)
-
 export const metadata: Metadata = {
 	title: 'Dashboard'
 }
 
 export default function DashBoard() {
-	return (
-		<div>
-			<Description />
-		</div>
-	)
+	return <div>Dashboard</div>
 }
