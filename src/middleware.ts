9f75f3b0caca4a38fe2e44c512db50286c00980a
Ayaz Mubarak <ayaz.mubarakov@gmail.com>
2024-08-24 09:46:50 +0300

    home url added

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,6 +9,7 @@ export async function middleware(request: NextRequest, response: NextResponse) {
 	// const refreshToken = cookies.get(EnumTokens.REFRESH_TOKEN)?.value
 	const accessToken = cookies.get(EnumTokens.ACCESS_TOKEN)?.value
 	const isAuthPage = url.includes('/auth')
+	const isHomePage = url.includes('/home')
 	const isInvite = url.includes('/invite')
 
 	if (isInvite) {
@@ -27,22 +28,22 @@ export async function middleware(request: NextRequest, response: NextResponse) {
 		}
 	}
 
-	if (isAuthPage && accessToken) {
+	if (accessToken && (isAuthPage || isHomePage)) {
 		return NextResponse.redirect(new URL(DASHBOARD_PAGES.DASHBOARD, url))
 	}
 
-	if (isAuthPage) {
+	if (isAuthPage || isHomePage) {
 		return NextResponse.next()
 	}
 
 	if (!accessToken) {
 		cookies.delete(EnumTokens.ACCESS_TOKEN)
-		return NextResponse.redirect(new URL('/auth', url))
+		return NextResponse.redirect(new URL('/home', url))
 	}
 
 	return NextResponse.next()
 }
 
 export const config = {
-	matcher: ['/dashboard/:path*', '/auth/:path', '/invite']
+	matcher: ['/dashboard/:path*', '/auth/:path', '/invite', '/home']
 }
